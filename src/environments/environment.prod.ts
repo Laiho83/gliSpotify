d9bfc5f22a5bdc15a18146cc2db2339a1ca9a853
Laiho83 <samir.kabir83@gmail.com>
2020-10-23 07:59:27 +0200

    Adding envirnments.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiurl: 'http://localhost:4200/',
+  production: true,
+  apiurl: 'https://laiho83.github.io/gliSpotify/',
   clientId: '03047c6f03f745b281bbdb44166f4ef7',
   clientSecret: 'edc4a0cc5ca64468bc5fff7580d658a2',
 };
