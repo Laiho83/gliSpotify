6c907f63b772617875636eb6a48d7473f7a6c6bc
Laiho83 <samir.kabir83@gmail.com>
2020-10-23 06:17:34 +0200

    Back button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, Event, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  albumNavActive: string = '';
 
-  ngOnInit(): void {
+  constructor(private router: Router) { 
+    router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        if(this.router.url.includes('/artist/album/')) {
+          const temp = this.router.url.substr(this.router.url.lastIndexOf('/')+1).split(':')[0];
+          this.albumNavActive = this.router.url.split('album/')[0]+temp;
+        } else {
+          this.albumNavActive = '';
+        }
+      }
+    })
+  }
+
+  ngOnInit() { 
   }
 
 }
